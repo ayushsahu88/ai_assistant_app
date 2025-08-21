ab273b6f8e61e683cc2e9525d53cb7304a5ccd8e
ayushsahu88 <ayushsahu871@gmail.com>
2025-08-21 21:54:26 +0530

    Update UserContext.jsx

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -4,7 +4,7 @@ import React, { createContext, useEffect, useState } from "react";
 export const userDataContext = createContext();
 
 const UserContext = ({ children }) => {
-  const serverUrl = "https://ai-assistant-app-n1cb.onrender.com";
+  const serverUrl = "https://ai-assistant-app-server.onrender.com";
   const [userData, setUserData] = useState(null);
   const [frontendImage, setFrontendImage] = useState(null);
   const [backendImage, setBackendImage] = useState(null);
